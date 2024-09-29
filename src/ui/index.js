845e54994eb803b5cc44c41a4f4011b01d625cd2
Luis Dale <luis.gascon@secured.team>
2024-09-28 23:12:31 -0400

    add image to document

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,19 +1,29 @@
 import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
+// import { add_image } from "./sdkUtils";
 
+function add_image(url) {
+    addOnUISdk.ready.then(() => {
+        async function callWhenReady() {
+            const blob = await fetch(url).then((r) => r.blob());
+            addOnUISdk.app.document.addImage(blob);
+        }
+        callWhenReady();
+    });
+}
 addOnUISdk.ready.then(async () => {
-    console.log("addOnUISdk is ready for use.");
-
     // Get the UI runtime.
     const { runtime } = addOnUISdk.instance;
 
     // Get the proxy object, which is required
     // to call the APIs defined in the Document Sandbox runtime
     // i.e., in the `code.js` file of this add-on.
-    const sandboxProxy = await runtime.apiProxy("documentSandbox");
-
+    // const sandboxProxy = await runtime.apiProxy("documentSandbox");
+    const route = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/cocaine/PNG';
     const createRectangleButton = document.getElementById("createRectangle");
     createRectangleButton.addEventListener("click", async event => {
-        await sandboxProxy.createRectangle();
+        console.log(route);
+        add_image(route);
+        // await sandboxProxy.getImage();
     });
 
     // Enable the button only when:
