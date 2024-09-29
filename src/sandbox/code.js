63169e823a8cfdfba325475582d1e4ed8ccfc480
Luis Dale <luis.gascon@secured.team>
2024-09-28 23:24:33 -0400

    clean up

--- src/sandbox/code.js
+++ src/sandbox/code.js
@@ -1,30 +1,7 @@
 import addOnSandboxSdk from "add-on-sdk-document-sandbox";
-import { editor} from "express-document-sdk";
-// Get the document sandbox runtime.
 const { runtime } = addOnSandboxSdk.instance;
 
 function start() {
-    // APIs to be exposed to the UI runtime
-    // i.e., to the `index.html` file of this add-on.
-    const sandboxApi = {
-        getImage: async () => {
-            const route = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/cocaine/PNG';
-            try {
-                const res = await fetch(route);
-                const blob = await res.blob();
-                // const imageUrl = URL.createObjectURL(blob);
-                const image = editor.createImageContainer(editor.loadBitmapImage(blob));
-                add_image(route)
-            } catch (err) {
-                console.log(err)
-                throw err;
-            }
-        }
-    };
-
-    // Expose `sandboxApi` to the UI runtime.
     runtime.exposeApi(sandboxApi);
-
     start();
-
 }
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,5 +1,4 @@
 import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
-// import { add_image } from "./sdkUtils";
 
 function add_image(url) {
     addOnUISdk.ready.then(() => {
@@ -10,25 +9,14 @@ function add_image(url) {
         callWhenReady();
     });
 }
-addOnUISdk.ready.then(async () => {
-    // Get the UI runtime.
-    const { runtime } = addOnUISdk.instance;
 
-    // Get the proxy object, which is required
-    // to call the APIs defined in the Document Sandbox runtime
-    // i.e., in the `code.js` file of this add-on.
-    // const sandboxProxy = await runtime.apiProxy("documentSandbox");
+addOnUISdk.ready.then(async () => {
+
     const route = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/cocaine/PNG';
     const createRectangleButton = document.getElementById("createRectangle");
     createRectangleButton.addEventListener("click", async event => {
-        console.log(route);
         add_image(route);
-        // await sandboxProxy.getImage();
     });
 
-    // Enable the button only when:
-    // 1. `addOnUISdk` is ready,
-    // 2. `sandboxProxy` is available, and
-    // 3. `click` event listener is registered.
     createRectangleButton.disabled = false;
 });
